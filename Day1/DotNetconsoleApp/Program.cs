95681dd1640e089d1f90dff0506c44d7c79997aa
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-02-24 10:41:39 +0530

    comit

---
+++ Day1/DotNetconsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetconsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registration Form");
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Your Password");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Mobile Number");
+            string mobile = Console.ReadLine();
+            Console.WriteLine($"Name{name}\n Age{age}\nEmail{email}\n sucessfully registered"  );
+
+        }
+    }
+}
---
+++ Day1/assignment1/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace assignment1
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            # region Scenario1 Student Management System
+            Console.WriteLine("Enter Student Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Student Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Percentage");
+            double percentage = double.Parse(Console.ReadLine());
+            Console.WriteLine("--Student Details--");
+            Console.WriteLine($"Name: {name}\nAge: {age}\nPercentage: {percentage}");
+            #endregion
+
+            #region Scenario2 Student Management System
+            Console.WriteLine("Enter New Student Name");
+            string newname = Console.ReadLine();
+            Console.WriteLine("Enter New Student Age");
+            int newage;
+            bool isAgeValid = int.TryParse(Console.ReadLine(), out newage);
+            if (!isAgeValid)
+            {
+                Console.WriteLine("Invalid Age");
+                return;
+            }
+            Console.WriteLine("\n--- Student Details ---");
+            Console.WriteLine($"Name: {newname}");
+            Console.WriteLine($"Age: {newage}");
+            #endregion
+
+            #region Scenario3 Student Management System
+            Console.WriteLine("Enter Student Name");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("Enter Student Age");
+            int age1;
+            bool isValid = int.TryParse(Console.ReadLine(), out age1);
+            if (!isValid)
+            {
+                Console.WriteLine("Invalid Age");
+                return;
+            }
+            Console.WriteLine("Enter Email");
+            string email = Console.ReadLine();
+            if (email == "" || email.Length == 0)
+            {
+                Console.WriteLine("email field cannot be empty");
+                return;
+            }
+            #endregion
+
+            #region Hospital management system
+
+            Console.WriteLine("Enter Patient Name:");
+            string patientName = Console.ReadLine();
+
+            Console.WriteLine("Please enter discharge date of patient: ");
+            string? dischargeDate = Console.ReadLine();
+
+            if (dischargeDate?.Length > 0) 
+            {
+                Console.WriteLine($"Discharge Date is: {dischargeDate}");
+            }
+            else
+            {
+                Console.WriteLine("Not Discharged.");
+            }
+            #endregion
+        }
+    }
+}
